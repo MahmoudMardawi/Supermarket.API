14da99065e9851521fa9820ee580ab4adb5812a3
MahmoudMardawi <ma.mardawi@gmail.com>
2022-11-28 14:03:42 +0200

    Inject mapper into CategoriesController to handle the objects mapping

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using Supermarket.API.Domain.Models;
 using Supermarket.API.Domain.Services;
+using Supermarket.API.Resources;
 
 namespace Supermarket.API.Controllers
 {
@@ -10,16 +12,21 @@ namespace Supermarket.API.Controllers
     public class CategoriesController : Controller
     {
         private readonly ICategoryService _categoryService;
+        private readonly IMapper _mapper;
 
-        public CategoriesController(ICategoryService categoryService)
+        public CategoriesController(ICategoryService categoryService, IMapper mapper)
         {
             _categoryService = categoryService;
+            _mapper = mapper;
+
         }
 
         [HttpGet]
         public async Task<IEnumerable<Category>> GetAllAsync()
         {
-            var categories = await _categoryService.ListAsync();
+            var categories = await _categoryService.ListAsync(); 
+            var resources = _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryResource>>(categories);
+
             return categories;
         }
     }
