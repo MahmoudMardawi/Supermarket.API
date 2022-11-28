52f31dcd2fd0e5703dbc34aeef4ef457239187d7
MahmoudMardawi <ma.mardawi@gmail.com>
2022-11-28 13:38:32 +0200

    add non-null value to the name in models

--- Domain/Models/Product.cs
+++ Domain/Models/Product.cs
@@ -3,11 +3,11 @@
     public class Product
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string ?Name { get; set; }
         public short QuantityInPackage { get; set; }
         public EUnitOfMeasurement UnitOfMeasurement { get; set; }
 
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category ?Category { get; set; }
     }
 }
